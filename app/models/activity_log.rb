63d611e1def985093f69c2a934eea328a70f91c9
conspirator <conspirator@cms.com>
2026-01-02 14:49:00 -0800

    more cleaning

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -1,4 +1,9 @@
 class ActivityLog < ApplicationRecord
   belongs_to :report
+  
+  # --- ADD THIS MISSING LINE ---
+  belongs_to :user
+  # -----------------------------
+  
   validates :note, presence: true
 end
